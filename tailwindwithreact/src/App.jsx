0ecc5a1a255be5dadf76883e1dca542ef8455bb4
Maadeha shaikh <maadeham@gmail.com>
2024-06-14 18:03:40 +0500

    Updated

--- tailwindwithreact/src/App.jsx
+++ tailwindwithreact/src/App.jsx
@@ -1,17 +1,25 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
-import BlueBtn from './components/BlueBtn'
-import GreenBtn from './components/GreenBtn'
-import PurpleBtn from './components/PurpleBtn'
 
 function App() {
+  const [color,setColor] = useState("olive");
   return (
-    <div className='app'>
-      <h1>Changing BackGround With React </h1>
-      <BlueBtn/>
-      <GreenBtn/>
-      <PurpleBtn/>
-    </div>
+    <>
+    
+     <div className='w-full h-screen' style={{backgroundColor:color}}>
+     <h1 className='w-full p-20'>Welcome to BackGround Changer !! </h1>
+      <div className='fixed flex flex-wrap justify-center bottom-12 insert-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+         <button className='outline-none px-3 py-1 rounded-full text-white shadow-sm' onClick={() => setColor('red')} style={{backgroundColor:'red'}}>Red</button>
+         <button className='outline-none px-3 py-1 rounded-full text-white shadow-sm' onClick={() => setColor('purple')} style={{backgroundColor:'purple'}}>purple</button>
+         <button className='outline-none px-3 py-1 rounded-full text-white shadow-sm' onClick={() => setColor('orange')} style={{backgroundColor:'orange'}}>orange</button>
+         <button className='outline-none px-3 py-1 rounded-full text-white shadow-sm' onClick={() => setColor('green')}  style={{backgroundColor:'green'}} >green</button>
+         <button className='outline-none px-3 py-1 rounded-full text-white shadow-sm' onClick={() => setColor('blue')} style={{backgroundColor:'blue'}}>blue</button>
+        </div>
+      </div>
+     </div>
+    </>
+     
   )
 }
 
