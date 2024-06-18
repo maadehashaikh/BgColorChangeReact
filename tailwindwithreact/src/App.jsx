a564cbe0d5a176e26593b18248b024f647f44082
Maadeha shaikh <maadeham@gmail.com>
2024-06-18 22:44:58 +0500

    Updated

--- tailwindwithreact/src/App.jsx
+++ tailwindwithreact/src/App.jsx
@@ -5,7 +5,6 @@ function App() {
   const [color,setColor] = useState("olive");
   return (
     <>
-     {color}
      <div className='w-full h-screen' style={{backgroundColor:color}}>
      <h1 className='w-full p-20'>Welcome to BackGround Changer !! </h1>
       <div className='fixed flex flex-wrap justify-center bottom-12 insert-x-0 px-2'>
@@ -14,9 +13,8 @@ function App() {
          <button className='outline-none px-3 py-1 rounded-full text-white shadow-sm' onClick={() => setColor('purple')} style={{backgroundColor:'purple'}}>purple</button>
          <button className='outline-none px-3 py-1 rounded-full text-white shadow-sm' onClick={() => setColor('orange')} style={{backgroundColor:'orange'}}>orange</button>
          <button className='outline-none px-3 py-1 rounded-full text-white shadow-sm' onClick={() => setColor('green')}  style={{backgroundColor:'green'}} >green</button>
-         <button className='outline-none px-3 py-1 rounded-full text-white shadow-sm' onClick={() => setColor('blue')} style={{backgroundColor:'blue'}}>blue</button>
+         <button className='outline-none px-3 py-1 rounded-full text-white shadow-sm' onClick={() => setColor('blue')} style={{backgroundColor:'orange'}}>orange</button>
         </div>
-        {color}
       </div>
      </div>
     </>
